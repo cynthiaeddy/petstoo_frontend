cfcf13aebeb25722e3012d9ef6879b8397aebcbd
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-04-03 14:37:04 -0400

    cleaned up

--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,62 +1,64 @@
-import React from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 class LoginForm extends React.Component {
-	state = {
-		name: '',
-		password: ''
-	};
+  state = {
+    name: '',
+    password: '',
+  }
 
-	handleChange = (event) => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-	};
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
 
-	handleSubmit = () => {
-		// fetch('http://localhost:3000/api/v1/login', {
-		// fetch(`https://petstoo-api.netlify.app/api/v1/login`, {
-		fetch(`https://pacific-hollows-81769.herokuapp.com/api/v1/login`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Accepts: 'application/json'
-			},
-			body: JSON.stringify(this.state)
-		})
-			.then((res) => res.json())
-			.then((resp) => {
-				if (resp.errors) {
-					// if there are errors the resp is an error
-					alert(resp.errors);
-				} else {
-					// if there are no errors, great, the resp is a user object
-					this.props.setCurrentUser(resp);
-				}
-			});
-	};
+  handleSubmit = () => {
+    // fetch('http://localhost:3000/api/v1/login', {
+    fetch(`https://pacific-hollows-81769.herokuapp.com/api/v1/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accepts: 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        if (resp.errors) {
+          alert(resp.errors)
+        } else {
+          this.props.setCurrentUser(resp)
+        }
+      })
+  }
 
-	render() {
-		return (
-			<Form onSubmit={this.handleSubmit}>
-				<Form.Field>
-					<label>name</label>
-					<input onChange={this.handleChange} name="name" value={this.state.name} autoComplete="off" />
-				</Form.Field>
-				<Form.Field>
-					<label>password</label>
-					<input
-						onChange={this.handleChange}
-						type="password"
-						name="password"
-						value={this.state.password}
-						autoComplete="off"
-					/>
-				</Form.Field>
-				<Button type="submit">Submit</Button>
-			</Form>
-		);
-	}
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Field>
+          <label>name</label>
+          <input
+            onChange={this.handleChange}
+            name="name"
+            value={this.state.name}
+            autoComplete="off"
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>password</label>
+          <input
+            onChange={this.handleChange}
+            type="password"
+            name="password"
+            value={this.state.password}
+            autoComplete="off"
+          />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    )
+  }
 }
 
-export default LoginForm;
+export default LoginForm
